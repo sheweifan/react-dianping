103f863d869186af2e9885174fda6081a336344b
dolor <923986234@qq.com>
2017-08-07 18:03:43 +0800

    fix config error

--- src/index.jsx
+++ src/index.jsx
@@ -9,24 +9,12 @@ import AppRouter from './router/index';
 import './static/styles/normalize.css';
 import './static/styles/common.css';
 
-
+/* eslint-disable no-undef */
 window.__DEV__ = process.env.NODE_ENV === 'development';
 console.log('开发环境', __DEV__);
-
 
 ReactDOM.render(
   <Provider store={store()}>
     <AppRouter />
   </Provider>,
   document.getElementById('ShiSheBuShiYu'));
-
-
-// let isMobile = false;
-// if ((/android/i).test(navigator.userAgent) || (/iphone|ipad/i).test(navigator.userAgent)) {
-//   const meta = document.querySelector('meta[name="viewport"]');
-//   const content = (meta as any).content;
-//   if (!(/initial-scale=1,/g.test(content))) {
-//     isMobile = true;
-//   }
-// }
-// windo.dpr = isMobile ? (window.devicePixelRatio || 1) : 1;
