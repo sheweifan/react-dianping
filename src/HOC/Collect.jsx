476147efb8caf6b6de0080559a86dfc701f0c1c9
dolor <923986234@qq.com>
2017-08-13 20:40:56 +0800

    eslint format

--- src/HOC/Collect.jsx
+++ src/HOC/Collect.jsx
@@ -13,58 +13,9 @@ const CollectHOC = WrappedComponent => class extends Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      collected: props.collected
+      collected: props.collected,
     };
     this.handleClick = this.handleClick.bind(this);
-  }
-  updateCollectState() {
-    const { userInfo, detailId } = this.props;
-    postData(collectUrl, {
-      _id: detailId,
-      userId: userInfo.openId,
-    })
-      .then((data) => {
-        const { isOk, changeSuccess } = data;
-        if (isOk) {
-          if (changeSuccess) {
-            this.setState({
-              collected: !this.state.collected,
-            });
-          }
-        }
-      });
-  }
-  handleClick() {
-    const { userInfo } = this.props;
-    const { collected } = this.state;
-    const { router } = this.context;
-    const self = this;
-    if (Object.keys(userInfo).length === 0) {
-      // 未登陆
-      Toast.fail('请先登录', 2, () => {
-        router.history.push('/login');
-      }, true);
-    } else {
-      // 已登陆
-      if (collected) {
-        // 已收藏
-        alert('收藏', '确定取消收藏？', [
-          {
-            text: '取消',
-          },
-          {
-            text: '确认',
-            onPress: () => {
-              // console.log('ok')
-              self.updateCollectState();
-            },
-          },
-        ]);
-      } else {
-        // 未收藏
-        this.updateCollectState();
-      }
-    }
   }
   render() {
     const { userInfo } = this.props;
@@ -82,6 +33,50 @@ const CollectHOC = WrappedComponent => class extends Component {
     this.setState({
       collected: nextprops.collected,
     });
+  }
+  updateCollectState() {
+    const { userInfo, detailId } = this.props;
+    postData(collectUrl, {
+      _id: detailId,
+      userId: userInfo.openId,
+    })
+      .then((data) => {
+        const { isOk, changeSuccess } = data;
+        if (isOk && changeSuccess) {
+          this.setState({
+            collected: !this.state.collected,
+          });
+        }
+      });
+  }
+  handleClick() {
+    const { userInfo } = this.props;
+    const { collected } = this.state;
+    const { router } = this.context;
+    const self = this;
+    if (Object.keys(userInfo).length === 0) {
+      // 未登陆
+      Toast.fail('请先登录', 2, () => {
+        router.history.push('/login');
+      }, true);
+    } else if (collected) {
+      // 已登陆 && 已收藏
+      alert('收藏', '确定取消收藏？', [
+        {
+          text: '取消',
+        },
+        {
+          text: '确认',
+          onPress: () => {
+            // console.log('ok')
+            self.updateCollectState();
+          },
+        },
+      ]);
+    } else {
+      // 未收藏
+      this.updateCollectState();
+    }
   }
 };
 
