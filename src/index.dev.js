cab5023cd9ee2cca598f1a6b25eee0d6eb0a4c8f
weifan she <923986234@qq.com>
2017-08-17 12:01:02 +0800

    format

--- src/index.dev.js
+++ src/index.dev.js
@@ -1,24 +1,4 @@
-// import _ from 'lodash';
-// function component() {
-//   var element = document.createElement('div');
-//   // Lodash, now imported by this script
-//   element.innerHTML = _.join(['Hello', 'webpack', 'haha', 'hehe',MyLibrary], ' ');
-//   element.classList.add('hello');
-//   return element;
-// }
-// document.body.appendChild(component());
-//
-
-// import 'src/style.less';
-// import React, { Component, PropTypes } from 'react';
-// import { render } from 'react-dom';
-
-// import Test from 'src/Test';
-
-// render(<Test />, document.getElementById('root'));
-
 import React from 'react';
-// import 'src/style.less';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 
--- src/index.prod.js
+++ src/index.prod.js
@@ -1,6 +1,5 @@
 
 import React from 'react';
-// import 'src/style.less';
 import ReactDOM from 'react-dom';
 
 const rootDom = document.getElementById('ShiSheBuShiYu');
